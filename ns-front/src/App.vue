1d03208c4ff38f90992e5cea3232653ce748943a
O. Saifutdinov <o.saifutdinov@iml.ru>
2020-11-27 20:30:01 +0300

    Created frontend base

---
+++ ns-front/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" class="container">
+    <login-page/>
+  </div>
+</template>
+
+<script>
+import LoginPage from './components/LoginPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LoginPage
+  }
+}
+</script>
+
+<style>
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+</style>
---
+++ ns-front/src/components/LoginPage.vue
@@ -0,0 +1,82 @@
+<template>
+    <div
+        class="login-form"
+    >
+        <div
+            class="title"
+        >
+            Авторизация
+        </div>
+        <div
+            class="credentials"
+        >
+            <i 
+                class="material-icons small valign"
+                title="Имя пользователя"
+            >
+                person
+            </i>
+            <input type="text">
+        </div>
+        <div
+            class="credentials"
+        >
+            <i
+                class="material-icons"
+                title="Пароль"
+            >
+                lock
+            </i>
+            <input type="password">
+        </div>
+        <div>
+            <button>
+                Войти
+            </button>
+        </div>
+        <div
+            class="footer-login"
+        >
+            <a href="/register">Регистрация</a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LoginPage',
+        data: () => ({
+            login: '',
+            password: ''
+        })
+    }
+</script>
+
+<style scoped>
+    .footer-login {
+        padding-right: 1em;
+        justify-content: flex-end;
+        display: inline-flex;
+    }
+    .credentials {
+        display: inline-flex;
+        vertical-align: middle;
+        align-items: center;
+        margin: .3em;
+        justify-content: center;
+    }
+    .title {
+        margin: .5em;
+        color: #424242;
+    }
+    .login-form {
+        display: flex;
+        flex-direction: column;
+        width: 30em;
+        height: 10em;
+        background-color: #E3F2FD;
+        box-shadow: 0.4em 0.4em 5px rgba(122,122,122,0.5);
+        border-radius: .3em;
+        font-family: Roboto;
+    }
+</style>
