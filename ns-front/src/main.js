325e5481323d203c68233eaf14caa7fc5682350c
O. Saifutdinov <o.saifutdinov@iml.ru>
2020-11-28 05:04:13 +0300

    Added vuex store

--- ns-front/src/main.js
+++ ns-front/src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import {createRouter} from './router'
 const router = createRouter()
+import { store } from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
