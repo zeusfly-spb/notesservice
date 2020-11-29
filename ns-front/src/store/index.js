e1a1c1fed3ab00f1b25ce12a6093cce160bc7e7b
O. Saifutdinov <o.saifutdinov@iml.ru>
2020-11-29 14:21:41 +0300

    Added files

--- ns-front/src/store/index.js
+++ ns-front/src/store/index.js
@@ -12,6 +12,12 @@ export const store = new Vuex.Store({
             return new Promise((resolve, reject) => {
                 Vue.axios.post('/register', {...data})
                     .then(res => {
+                        if (res.data.error) {
+                            if (res.data.error.includes('Unique')) {
+                                alert('Такой логин уже занят')
+                            }
+                            reject(res.data.error)
+                        }
                         commit('SET_REGISTERED_USER', res.data)
                         resolve(res)
                     })
